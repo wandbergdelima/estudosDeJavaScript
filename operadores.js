2708739102a97b995387c1d90a1ad1c15448b11f
wandbergdelima <wandberg@gmail.com>
2022-07-24 21:01:27 -0300

    trabalhando com operadores Logicos

---
+++ operadores.js
@@ -0,0 +1,7 @@
+if (true) {
+    document.write('Entrou dentro do bloco if (Verdadeiro)')    
+} 
+
+// else {
+    
+// }
--- variaveis.js
+++ variaveis.js
@@ -3,7 +3,7 @@ var teste = 'string ou texto'; // tipo textos
 var teste2 = -5; // tipo number
 var teste3 = 3.14; // tipo float ou double
 var teste4 = true; // verdadeiro ou falso (false)
-var teste5 = null; // Sem valor definido
+var teste5 = null; // Sem um valor definido ou ausencia intencional de um valor
 console.log(teste5);
 teste5 = 'valor modificado' // texto
 console.log(teste5);
@@ -12,7 +12,7 @@ console.log(teste5);
 teste5 = 'valor modificado' // texto
 
 console.log('Testes com variaveis undefined');
-var teste6; // undefined
+var teste6; // undefined ou indefinido geralmente aparece em retornos de erros
 console.log(teste6);
 teste6 = "valor modificado"
 console.log(teste6);
